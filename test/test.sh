ada59117238be9589548a1893662a293860104a1
benoit <benoit.lobreau@dalibo.com>
2020-08-20 14:47:06 +0200

    Test updates

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,10 @@ log() {
 	echo "$(date) LOG $*"
 }
 
+warn() {
+	echo "$(date) LOG $*"
+}
+
 die() {
 	echo "$(date) FATAL $*"
 	exit 1
@@ -14,12 +18,14 @@ die() {
 bootstrap_instance (){
 	local PGBIN=$1
 	local PGDATA=$2
-	local TEST=$3
+	local TESTFILE=$3
+
 	local CUSTOM_VARIABLE_CLASS=""
 	local SOCKET_DIR="unix_socket_directories"
 
 	log "initdb -D $PGDATA"
 	"$PGBIN"/initdb -D "$PGDATA" --username postgres &>/dev/null
+	#"$PGBIN"/initdb -D "$PGDATA" --username postgres
 
 	VERSION=$(cat "$PGDATA/PG_VERSION")
 	if [[ "$VERSION" == "9.1" ]] || [[ "$VERSION" == "9.0" ]]; then
@@ -42,11 +48,11 @@ $CUSTOM_VARIABLE_CLASS
 pg_log_userqueries.log_level = 'INFO'
 pg_log_userqueries.log_label = 'LOG_UQ'
 EOF
-
-	cat ./sql/$TEST.conf >>"$PGDATA"/postgresql.conf
+	cat ./sql/${TESTFILE}.conf >> "$PGDATA"/postgresql.conf
 
 	log "pgstart $PGDATA"
 	"$PGBIN"/pg_ctl start -D "$PGDATA" -w &>/dev/null
+	#"$PGBIN"/pg_ctl start -D "$PGDATA" -w
 }
 
 scratch_instance (){
@@ -59,6 +65,7 @@ scratch_instance (){
 
 	log "pgstop $PGDATA"
 	"$PGBIN"/pg_ctl stop -D "$PGDATA" -w -m fast &>/dev/null
+	#"$PGBIN"/pg_ctl stop -D "$PGDATA" -w -m fast
 	log "removing $PGDATA"
 	rm -Rf "$PGDATA"
 }
@@ -70,22 +77,30 @@ status_instance (){
 
 run_test (){
 	local PGBIN=$1
-	local TEST=$2
+	local TESTFILE=$2
 
+#	local VERSION=""
 	log "test"
-	$PGBIN/psql -Xe -p $PORT -U postgres -d postgres -f ./sql/$TEST.sql
+
+	$PGBIN/psql -Xe -p $PORT -U postgres -d postgres -f ./sql/${TESTFILE}.sql
+
+#	VERSION=$($PGBIN/psql -p $PORT -U postgres -d postgres -XtA -c "SELECT substring(lpad(current_setting('server_version_num'),6,'0'), 1, 4)")
+#	$PGBIN/psql -Xe -p $PORT -U postgres -d postgres -f ./sql/${TESTFILE}.sql 2>./test.log
+
+#	diff ./sql/${TESTFILE}_${VERSION}.log ./test.log 2>/dev/null
+#	[[ "$?" -ne "0" ]] && warn "Test $TESTFILE for version $VERSION failed"
 }
 
 instance_test() {
 	local PGBIN="$1"
 	local PGDATA="$2"
-	local TEST="$3"
+	local TESTFILE="$3"
 
 	log "==== test version : $($PGBIN/postmaster -V)"
 
-	bootstrap_instance "$PGBIN" "$PGDATA" "$TEST"
+	bootstrap_instance "$PGBIN" "$PGDATA" "$TESTFILE"
 	status_instance "$PGBIN" "$PGDATA"
-	run_test "$PGBIN" "$TEST"
+	run_test "$PGBIN" "$TESTFILE"
 	scratch_instance "$PGBIN" "$PGDATA"
 }
 
